7600b6a55906fac536d65611ce0a6d7a1efbd3fd
Adhiambo Peres <adhiambo@sendyit.com>
2020-08-17 19:07:11 +0300

    autowire the listener

--- src/main/java/com/example/demo/Receiver.java
+++ src/main/java/com/example/demo/Receiver.java
@@ -1,21 +1,14 @@
 package com.example.demo;
 
-import java.util.concurrent.CountDownLatch;
-
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
-    private CountDownLatch latch = new CountDownLatch(1);
-
+    @RabbitListener(queues = "spring-boot")
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
-        latch.countDown();
-    }
-
-    public CountDownLatch getLatch() {
-        return latch;
     }
 }
 
--- src/main/java/com/example/demo/Runner.java
+++ src/main/java/com/example/demo/Runner.java
@@ -4,16 +4,12 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
-
 @Component
 public class Runner implements CommandLineRunner {
 
     private final RabbitTemplate rabbitTemplate;
-    private final Receiver receiver;
 
-    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
-        this.receiver = receiver;
+    public Runner(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
@@ -21,7 +17,6 @@ public class Runner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("Sending message...");
         rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
     }
 
 }
